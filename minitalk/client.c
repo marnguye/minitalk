98083b30f64b5800ce6b1c4d636a9e00ef478fa5
martin <144384507+marnguye@users.noreply.github.com>
2024-02-23 16:29:46 +0100

    Add files via upload

--- minitalk/client.c
+++ minitalk/client.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnguye <marnguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 13:05:35 by marnguye          #+#    #+#             */
+/*   Updated: 2024/02/23 14:44:53 by marnguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-	int result;
-	int sign;
+	int	result;
+	int	sign;
 
 	result = 0;
 	sign = 1;
@@ -18,37 +30,28 @@ int ft_atoi(const char *str)
 	return (result * sign);
 }
 
-void send_signal(int pid, char c)
+void	send_signal(int pid, char c)
 {
-	int arr[8];
-	int i;
-	int j;
+	send_bit(pid, c, 0);
+}
 
-	j = 128;
-	i = 7;
-	while (i >= 0)
+void	send_bit(int pid, char c, int i)
+{
+	if (i < 8)
 	{
-		if (c & j)
-			arr[i] = 1;
-		else
-			arr[i] = 0;
-		if (j > 0)
-			j >>= 1;
-		i--;
-	}
-	while (++i < 8)
-	{
-		if (arr[i] == 0)
-			kill(pid, SIGUSR1);
-		else
+		if (c & (1 << i))
 			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
 		usleep(100);
+		send_bit(pid, c, i + 1);
 	}
 }
 
-int main(int argc, char **argv)
+
+int	main(int argc, char **argv)
 {
-	int pid;
+	int	pid;
 
 	if (argc != 3)
 		write(1, "Error bro, something is wrong!", 31);
--- minitalk/minitalk.h
+++ minitalk/minitalk.h
@@ -1,11 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marnguye <marnguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/23 13:05:48 by marnguye          #+#    #+#             */
+/*   Updated: 2024/02/23 14:45:31 by marnguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
 
-void handle_signal(int signal);
-void send_signal(int pid, char c);
+void	handle_signal(int signal);
+void	send_signal(int pid, char c);
+void	send_bit(int pid, char c, int i);
 
 #endif
